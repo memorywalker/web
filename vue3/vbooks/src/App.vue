135dd74fb961ea7ea211ed68bde652f50656c050
memorywalker <memorywalker@163.com>
2024-05-04 19:38:04 +0800

    fullstack vue3 single file component source code

---
+++ vue3/vbooks/src/App.vue
@@ -0,0 +1,41 @@
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+import CalendarWeek from './components/CalendarWeek.vue';
+import CalendarEntry from './components/CalendarEntry.vue';
+
+export default {
+  name: 'App',
+  components: {
+    CalendarWeek,
+    CalendarEntry
+  }
+}
+</script>
+
+<template>
+  <div id="app">
+    <CalendarWeek />
+    <CalendarEntry />
+  </div>
+</template>
+
+<style lang="scss">
+html, body, #app {
+  height: 100%;
+  width: 100%;
+}
+</style>
+
+<style lang="scss" scoped>
+
+#app {
+  background: #6e6e6e;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  -webkit-align-items: center;
+  justify-content: center;
+  -webkit-justify-content: center;
+}
+</style>
